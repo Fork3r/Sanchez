6aee7fd776d3b297be0a31d6cca8b7cb688a346c
r-salogubov <r.salogubov@seraphim.online>
2019-01-11 17:55:08 +0300

    fixed test functional

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 #! /usr/bin/python
 from subprocess import Popen, PIPE
 from multiprocessing import Process, Queue
-import sys
-
-cmd = '/home/rsalogub/QTProjects/build-Sanchez-Desktop-Debug/Sanchez'
+import os
+cmd = os.path.join(os.getcwd()) + '/' + 'Sanchez'
 proc = Popen(cmd, stdout=PIPE, stdin=PIPE, stderr=PIPE)
 #while proc.poll() == None:
 proc.stdin.write(b"foo\n")
